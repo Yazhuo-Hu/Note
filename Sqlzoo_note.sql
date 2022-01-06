3b5b471dc9d8afd64364d5a570c40e9d31df4e47
Yazhuo-Hu <89788667+Yazhuo-Hu@users.noreply.github.com>
2022-01-07 07:18:07 +0800

    Update Sqlzoo_note.sql

--- Sqlzoo_note.sql
+++ Sqlzoo_note.sql
@@ -1,2 +1,6 @@
-- 8 
-Select goals scored only by non-German players in matches where GER was the id of either team1 or team2.
+-- - 8 
+-- Select goals scored only by non-German players in matches where GER was the id of either team1 or team2.
+SELECT DISTINCT player 
+FROM goal JOIN game ON goal.matchid = game.id
+WHERE (team1 = 'GER' OR team2 = 'GER')
+  AND teamid <> 'GER'
